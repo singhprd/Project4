964c3551953133807eb30bd9dd9a3b4990e01cd0
Peter Singh <singh.prd@gmail.com>
2016-05-21 23:46:52 +0100

    added styling to index.html and added content class from pure to the main react div

--- node_foodshare/client/src/index.js
+++ node_foodshare/client/src/index.js
@@ -5,15 +5,17 @@ var SupplyItemForm = require('./components/SupplyItemForm.jsx')
 
 window.onload = function() {
   console.log('node server has served this js file');
-  ReactDOM.render(
-    <Main url="http://localhost:3000/"></Main>, 
-    document.getElementById('app')
-  );
-  // Quick render of form too work on layout
   // ReactDOM.render(
-  //   <SupplyItemForm/>, 
+  //   <Main url="http://localhost:3000/"></Main>, 
   //   document.getElementById('app')
   // );
+
+  
+  // Quick render of form too work on layout
+  ReactDOM.render(
+    <SupplyItemForm/>, 
+    document.getElementById('app')
+  );
 
 };
 
