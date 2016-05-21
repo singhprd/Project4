51b3cb88564355e5e56bac470e5b88323e3a669d
Peter Singh <singh.prd@gmail.com>
2016-05-21 16:00:03 +0100

    Fixed route to users.json

--- rails_foodshare/config/routes.rb
+++ rails_foodshare/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :jobs
   resources :companies
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- rails_foodshare/db/seeds.rb
+++ rails_foodshare/db/seeds.rb
@@ -6,5 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
 
 
