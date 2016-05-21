db32674e4dfcf43bbc1a4a5d5bbbfc37be45c5f2
Peter Singh <singh.prd@gmail.com>
2016-05-21 17:23:56 +0100

    added welcome page and controller

--- rails_foodshare/config/routes.rb
+++ rails_foodshare/config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   resources :jobs
   resources :companies
   resources :users
+  resources :welcome
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'users#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
