8f6155633e610569e5bea47480c31d0e5d250652
claire <claire.mariko.smith@gmail.com>
2016-05-21 12:54:59 +0100

    dealt with CORS error

--- node_foodshare/server/server.js
+++ node_foodshare/server/server.js
@@ -3,11 +3,11 @@ var app = express();
 var path = require('path');
 
 app.use(express.static('../client/build'));
-// app.use(function(req, res,next){
-//   res.header('Access-Control-Allow-Origin', '*');
-// res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res,next){
+  res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
