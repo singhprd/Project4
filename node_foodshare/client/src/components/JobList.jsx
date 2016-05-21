b2be67b7a10200a54c452733d2e66e252f6a51eb
alilucybee <alison_lucy_barnett@hotmail.com>
2016-05-21 15:17:18 +0100

    made a joblist component

---
+++ node_foodshare/client/src/components/JobList.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var JobList = React.createClass({
+
+  
+
+  render:function(){
+    return(
+      <div>
+        <h4> My job list</h4>
+
+      </div>
+      )
+  }
+
+})
+
+
+module.exports = JobList;
