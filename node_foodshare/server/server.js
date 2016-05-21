e0d1501b7eb0fc30c33a5cebed4f57c6f0cf0a26
claire <claire.mariko.smith@gmail.com>
2016-05-21 12:36:30 +0100

    safe point

--- node_foodshare/server/server.js
+++ node_foodshare/server/server.js
@@ -3,6 +3,11 @@ var app = express();
 var path = require('path');
 
 app.use(express.static('../client/build'));
+// app.use(function(req, res,next){
+//   res.header('Access-Control-Allow-Origin', '*');
+// res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
@@ -11,6 +16,7 @@ app.get('/', function (req, res) {
 app.get('/helloworld', function(req, res) {
   res.send('hello world');
 });
+
 
 var server = app.listen(3001, function () {
   var host = server.address().address;
