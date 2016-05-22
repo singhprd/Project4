d6c98453064d10b06fb2122b839c277d1bc42ea2
alilucybee <alison_lucy_barnett@hotmail.com>
2016-05-22 15:29:57 +0100

    got a bit further displaying a map but needs more work

--- node_foodshare/client/src/components/GoogleMap.jsx
+++ node_foodshare/client/src/components/GoogleMap.jsx
@@ -1,21 +1,45 @@
 var React = require('react');
 
+// var Map = require('../map/googlemap');
 
 
 var GoogleMap = React.createClass({
 
+  createMap:function(){
+    var coords = this.props.coords;
+    var zoom = this.props.zoom;
+    var center = new google.maps.LatLng(coords.lat, coords.lng);
+    var canvas = this.refs["map_canvas"];
+    return (new google.maps.Map(canvas, {
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 8
+    }));
+
+  },
+
+  componentDidMount:function(){
+    this.createMap();
+  },
   
 
+
   render:function(){
+    
+
     return(
-      <div>
+      <div className= "map">
+      <h4>Map is a little bit here</h4>
+        <div ref="map_canvas" id="map_canvas">
+        </div>
+
+
       
-        <h4> My map</h4>
 
 
       </div>
       )
-  }
+      }
+  
 
 })
 
