9b1b2ed9e412334882f4ba585db973cdc662d4bc
A_Lippok <alippok@hotmail.co.uk>
2016-05-22 10:29:30 +0100

    Finishes basic Courier model and tests

--- rails_foodshare/app/models/courier.rb
+++ rails_foodshare/app/models/courier.rb
@@ -3,4 +3,17 @@ class Courier < ActiveRecord::Base
   def getFirstName
     return first_name
   end
+
+  def getLastName
+    return last_name
+  end
+
+  def getFullName
+    return "#{first_name} #{last_name}"
+  end
+
+  def getPhoneNumber
+    return phone
+  end
+
 end
--- rails_foodshare/test/models/courier_test.rb
+++ rails_foodshare/test/models/courier_test.rb
@@ -7,4 +7,17 @@ class CourierTest < ActiveSupport::TestCase
   test "has first name" do 
     assert_equal("Jenny", couriers(:one).getFirstName)
   end
+
+  test "has last name" do 
+    assert_equal("Bloggs", couriers(:one).getLastName)
+  end
+
+  test "can get full name" do 
+    assert_equal("Jenny Bloggs", couriers(:one).getFullName)
+  end
+
+  test "has a phone number" do 
+    assert_equal(07712343455, couriers(:one).getPhoneNumber)
+  end
+
 end
