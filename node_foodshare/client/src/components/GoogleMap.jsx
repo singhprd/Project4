3b02e2ebde7d7e40a1d9c22cefce0419f45cf6b3
alilucybee <alison_lucy_barnett@hotmail.com>
2016-05-23 15:47:47 +0100

    googlemap markers now display for multiple jobs and findme button creates geopositioned marker rather than new map

--- node_foodshare/client/src/components/GoogleMap.jsx
+++ node_foodshare/client/src/components/GoogleMap.jsx
@@ -35,17 +35,28 @@ var GoogleMap = React.createClass({
             navigator.geolocation.getCurrentPosition(function(position){
             var currentLat = position.coords.latitude;
             var currentLng = position.coords.longitude;
-            var userLocation = {lat: currentLat, lng: currentLng}
-            return (this.map = new google.maps.Map(canvas, {center:userLocation, zoom: 16}));
-            // map.addMarker( userLocation, "", "", "United Kingdom" );
+            var userLocation = {lat: currentLat, lng: currentLng};
+            // return (this.map = new google.maps.Map(canvas, {center:userLocation, zoom: 16}));
+            // this.addMarker( userLocation);
+
+            var marker = this.addMarker(userLocation, "images/purple-marker.png");
+            marker.addListener('click', function(){
+              var myInfoWindow = new google.maps.InfoWindow({
+                content: "I am here"
+            
+              });
+              myInfoWindow.open(this.map, marker);
+
             })
+          }.bind(this))
         
   },
 
-  addMarker:function(latLng){
+  addMarker:function(latLng, image){
       var marker = new google.maps.Marker({
         position: latLng,
         map: this.map,
+        icon: image
 
       });
       return marker;
@@ -87,31 +98,48 @@ var GoogleMap = React.createClass({
 
 
   render:function(){
-    //getting info from props but this is going to need more work for more than one job
-    var companyName = this.props.jobs.map(function(job){
-        return (job.company.name);
-    });
+    //mapping the jobs to show information on map for each one:
+    this.props.jobs.map(function(job){
+      //getting the lat and lng for each job and displaying marker for that location on the map:
+      var companyLat = job.company.position.lat;
+      var companyLng = job.company.position.lng; 
+      this.addMarker({lat: companyLat, lng: companyLng});
+      
+      //getting the company name and category for each job and populating the info window with these details:
+      var companyName = job.company.name;
+      var jobType = job.category;
+      var infoWindowContent = companyName.toString() + " " + jobType.toString();
+      return this.addInfoWindow({lat: companyLat, lng: companyLng}, infoWindowContent);
 
-    var companyLat = this.props.jobs.map(function(job){
-        return (job.company.position.lat);
-    });
+    }.bind(this));
 
-    var companyLng = this.props.jobs.map(function(job){
-        return (job.company.position.lng);
-    });
 
-    var jobType = this.props.jobs.map(function(job){
-        return (job.type);
-    });
+    //old way of doing it:
+
+    // var companyName = this.props.jobs.map(function(job){
+    //     return (job.company.name);
+    // });
+
+    // var companyLat = this.props.jobs.map(function(job){
+    //     return (job.company.position.lat);
+    // });
+
+    // var companyLng = this.props.jobs.map(function(job){
+    //     return (job.company.position.lng);
+    // });
+
+    // var jobType = this.props.jobs.map(function(job){
+    //     return (job.category);
+    // });
 
     //taking the mapped info into 
-    var infoWindowContent = companyName.toString() + " " + jobType.toString();
-    var companyLatMap = companyLat.pop();
-    var companyLngMap = companyLng.pop();
+    // var infoWindowContent = companyName.toString() + " " + jobType.toString();
+    // var companyLatMap = companyLat.pop();
+    // var companyLngMap = companyLng.pop();
        
-    this.addMarker({lat: companyLatMap, lng: companyLngMap});
+    // this.addMarker({lat: companyLatMap, lng: companyLngMap});
 
-    this.addInfoWindow({lat: companyLatMap, lng: companyLngMap}, infoWindowContent);
+    // this.addInfoWindow({lat: companyLatMap, lng: companyLngMap}, infoWindowContent);
 
     return(
       <div className= "map">
