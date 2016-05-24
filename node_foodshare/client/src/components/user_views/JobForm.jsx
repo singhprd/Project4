e00033ac4c6f1bb6d736c776a83ec84a01ed988b
Peter Singh <singh.prd@gmail.com>
2016-05-24 17:18:49 +0100

    styling

--- node_foodshare/client/src/components/user_views/JobForm.jsx
+++ node_foodshare/client/src/components/user_views/JobForm.jsx
@@ -1,17 +1,28 @@
-// http://purecss.io/forms/
-// I used these docs to make this form
+Date.prototype.isValid = function () {
+  // An invalid date object returns NaN for getTime() and NaN is the only
+  // object not strictly equal to itself.
+  return this.getTime() === this.getTime();
+};  
 
 var React = require('react');
 var LinkedStateMixin = require('react-addons-linked-state-mixin');
 
 var SupplyItemForm = React.createClass({
-    mixins: [LinkedStateMixin],
+  mixins: [LinkedStateMixin],
 
-    getInitialState: function() {
-      return {item:'',instructions:'',quantity:''}
-    },
+  getInitialState: function() {
+    return {item:'',instructions:'',quantity:'', from_date: '', to_date: '', error:''}
+  },
 
-    handleSubmit: function(e){
+  handleSubmit: function(e){
+    var a = new Date(this.state.from_date);
+    var b = new Date(this.state.to_date);
+
+    if(!a.isValid() || !b.isValid()) {
+      console.log("dates not valid")
+      this.setState({error:"Check Dates are valid"})
+      return
+    }
       e.preventDefault();
       var request = new XMLHttpRequest();
       request.open("POST", this.props.url+'jobs');
@@ -27,48 +38,50 @@ var SupplyItemForm = React.createClass({
       var data = {
         instructions:this.state.instructions,
         quantity:this.state.quantity,
-        item:this.state.item
+        item:this.state.item,
+        from_date: this.state.from_date,
+        to_date: this.state.to_date
       }
-    request.send(JSON.stringify(data));
+    // request.send(JSON.stringify(data));
   },
   render: function() {
     return (
       <div>
-        <form className="pure-form pure-form-aligned">
-        <fieldset>
-          <div className="pure-control-group">
-            <label for="item">item</label>
-            <input  valueLink={this.linkState('item')} id="item" type="text" placeholder="item"/>
-          </div>
+      <form className="pure-form pure-form-aligned">
+      <fieldset>
+      <div className="pure-control-group">
+      <label for="item">item</label>
+      <input  valueLink={this.linkState('item')} id="item" type="text" placeholder="item"/>
+      </div>
 
-          <div className="pure-control-group">
-            <label for="quantity">quantity</label>
-            <input valueLink={this.linkState('quantity')} id="quantity" type="text" placeholder="quantity"/>
-          </div>
+      <div className="pure-control-group">
+      <label for="quantity">quantity</label>
+      <input valueLink={this.linkState('quantity')} id="quantity" type="text" placeholder="quantity"/>
+      </div>
 
-          <div className="pure-control-group">
-            <label for="instructions">instructions</label>
-            <input valueLink={this.linkState('instructions')} id="instructions" type="text" placeholder="instructions"/>
-          </div>
+      <div className="pure-control-group">
+      <label for="instructions">instructions</label>
+      <input valueLink={this.linkState('instructions')} id="instructions" type="text" placeholder="instructions"/>
+      </div>
 
-          <div className="pure-control-group">
-            <label for="from_date">from_date</label>
-            <input valueLink={this.linkState('from_date')} id="from_date" type="date" placeholder="from_date"/>
-          </div>
-          <div className="pure-control-group">
-            <label for="to_date">to_date</label>
-            <input valueLink={this.linkState('to_date')} id="to_date" type="date" placeholder="to_date"/>
-          </div>
+      <div className="pure-control-group">
+      <label for="from_date">from_date</label>
+      <input valueLink={this.linkState('from_date')} id="from_date" type="date" placeholder="from_date"/>
+      </div>
+      <div className="pure-control-group">
+      <label for="to_date">to_date</label>
+      <input valueLink={this.linkState('to_date')} id="to_date" type="date" placeholder="to_date"/>
+      </div>
 
+      <div className="pure-controls">
+      <button onClick={this.handleSubmit} type="submit" className="pure-button pure-button-primary"> <i className="fa fa-check" aria-hidden="true"></i> Submit</button>
+      </div>
 
-          <div className="pure-controls">
-          <button onClick={this.handleSubmit} type="submit" className="pure-button pure-button-primary"> <i className="fa fa-check" aria-hidden="true"></i> Submit</button>
-          </div>
-
-        </fieldset>
+      </fieldset>
       </form>
-    </div>
-    )
+      {this.state.error}
+      </div>
+      )
   }
 });
 
